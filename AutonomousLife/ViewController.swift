bf834c53d0dcc7935a60934e579698a2e20801b6
Alex Ilyin <alexilyin@macbook-pro-65.local>
2018-10-15 09:26:06 +0200

    Initial Commit

---
+++ AutonomousLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutonomousLife
+//
+//  Created by Alex Ilyin on 15/10/2018.
+//  Copyright © 2018 Alex Ilyin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
